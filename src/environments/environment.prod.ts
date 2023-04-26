3f64441b6aee8984fb7d2bb9e19525aaedae9822
M4ty155 <matipollo98@gmail.com>
2023-04-25 21:22:53 -0300

    aplicando storage

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'portfoliofrontend-94602',
+    appId: '1:78768063767:web:0e1b605f6f331018eba352',
+    storageBucket: 'portfoliofrontend-94602.appspot.com',
+    apiKey: 'AIzaSyBDT8qR20SyEu8RTEEPNi6cwKbsBVKox0E',
+    authDomain: 'portfoliofrontend-94602.firebaseapp.com',
+    messagingSenderId: '78768063767',
+    measurementId: 'G-2F963H74WD',
+  },
   production: true,
 
   URL: 'https://considerable-cathie-m4ty155.koyeb.app/',
