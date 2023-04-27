f3ff7b18f663e3348acee92535a9517ca1789a21
M4ty155 <matipollo98@gmail.com>
2023-04-27 16:53:15 -0300

    agregando storage

--- src/app/service/image.service.ts
+++ src/app/service/image.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Storage, ref, uploadBytes, list, getDownloadURL } from '@angular/fire/storage';
-import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageService {
   url: string = "";
-  imgRef: string = ""
 
-  constructor(private storage: Storage, private spinner: NgxSpinnerService) { }
+  constructor(private storage: Storage) { }
 
   public uploadImage($event: any, name: string){
-    this.spinner.show();
     const file = $event.target.files[0]
     const imgRef = ref(this.storage, `imagen/`+ name)
     uploadBytes(imgRef, file)
-    .then(response => { this.imgRef = response.ref.fullPath;
-      this.getImages()})
+    .then(response => {this.getImages()})
     .catch(error => console.log(error))
   }
 
@@ -26,11 +22,8 @@ export class ImageService {
     list(imagesRef)
     .then(async response => {
       for(let item of response.items){
-        if(this.imgRef == item.fullPath){
         this.url = await getDownloadURL(item);
-        this.spinner.hide();
         console.log("La url es: " + this.url);
-        }
       }
     })
     .catch(error => console.log(error))
